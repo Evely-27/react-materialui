e7402ea0bbe0ed612c8bb12707ade7e571a79e48
Evely-27 <evelysantos27@gmail.com>
2022-11-25 14:46:18 -0300

    feat: craidando hook para lidar com delay,useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
+
 import { FerramentasDaListagem } from '../../shared/components';
+import { useDebounce } from '../../shared/hooks';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasServices';
 
@@ -8,21 +10,25 @@ import { PessoasService } from '../../shared/services/api/pessoas/PessoasService
 export const ListagemDePessoas:React.FC = ( ) => {
     //hook que nos retorna um array para poder usar mandar o textodebusca para o router
     const [ searchParams, setSearchParams] = useSearchParams();
+    const {debounce} = useDebounce();
 
     const busca= useMemo(( )=> {
         return searchParams.get('busca') || '';
     },[searchParams]);
 
-
+    // consulta para nosso bd
     useEffect(() => {
-        PessoasService.getAll(1,busca)
-            .then((result) => {
-                if (result instanceof Error) {
-                    alert(result.message);
-                } else {
-                    console.log(result);
-                }
-            });
+
+        debounce (() => {
+            PessoasService.getAll(1,busca)
+                .then((result) => {
+                    if (result instanceof Error) {
+                        alert(result.message);
+                    } else {
+                        console.log(result);
+                    }
+                });
+        });
     }, [busca]);
 
     return (
