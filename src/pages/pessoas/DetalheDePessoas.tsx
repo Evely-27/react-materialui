d3de475cc1fd9ce2fe5cfb074ba2129a773c5db8
Evely-27 <evelysantos27@gmail.com>
2022-11-30 15:15:39 -0300

    feat: create Detalhe de pessoas

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,7 +1,10 @@
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
+import { PessoasService } from '../../shared/services/api/pessoas/PessoasServices';
 import { FerramentasDeDetalhe } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
+import { LinearProgress } from '@mui/material';
 
 
 export const DetalheDePessoas: React.FC = () => {
@@ -10,8 +13,41 @@ export const DetalheDePessoas: React.FC = () => {
 
     const navigate = useNavigate();
 
-    const handleDelete = () => {
-        console.log('Delete');
+    const [isLoading,setIsLoading] = useState(false);
+    const [nome,setNome] = useState('');
+
+    // aqui trazemos os dados do registro que escolhemos
+    useEffect(() => {
+        // se o id for diferente do nova, vai buscar no bd
+        if (id !== 'nova') {
+            setIsLoading(true);
+
+            PessoasService.getById(Number(id))
+                .then((result) => {
+                    setIsLoading(false);
+                    if (result instanceof Error) {
+                        alert(result.message);
+                        navigate('/pessoas');
+                    } else { 
+                        setNome(result.nomeCompleto);
+                        console.log(result);
+                    }
+                });
+        }
+    },[id]); //o useEffet depende desse id, por isso entra na dependencia.
+
+    const handleDelete = (id:number) => {
+        if(confirm('Realmente quer apagar?')) {
+            PessoasService.deletById(id)
+                .then(result => {
+                    if(result instanceof Error) {
+                        alert(result.message);
+                    }else {
+                        alert('Registro apagado com sucesso!!');
+                        navigate('/pessoas');
+                    }
+                });
+        }
     };
 
     const handleSave = () => {
@@ -20,7 +56,7 @@ export const DetalheDePessoas: React.FC = () => {
 
     return (
         <LayoutBaseDePagina
-            titulo='Detalhe de pessoas'
+            titulo={id === 'nova' ? 'Nova pessoa': nome}
             barraDeFerramentas={
                 <FerramentasDeDetalhe 
                     mostrarBotaoSalvarEFechar
@@ -31,12 +67,17 @@ export const DetalheDePessoas: React.FC = () => {
 
                     aoClicarEmSalvarEFechar={handleSave}
                     aoClicarEmSalvar= {handleSave}
-                    aoClicarEmApagar={handleDelete}
+                    aoClicarEmApagar={() => handleDelete(Number(id))}
                     aoClicarEmVoltar= {() => navigate('/pessoas')}
                     aoClicarEmNovo= {() => navigate('/pessoas/detalhe/nova')}
                 />
             }
         >
+
+            {isLoading && (
+                <LinearProgress variant='indeterminate' />
+            )}
+
             <p>Detalhe de Pessoa {id}</p>
         </LayoutBaseDePagina>
        
